29a8d93b892a6212ed84a0c9216620349f66bfde
Thomas Varnish <thomasvarnish@yahoo.co.uk>
2016-08-09 23:05:55 +0100

    Cleaned up Body class

--- sombrero.cpp
+++ sombrero.cpp
@@ -1,40 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
 class Body {
 	double x, y;
 	double xVelocity, yVelocity;
 	double mass;
 public:
-	double GetMass();
-	double * GetPosition();
-	void SetPosition(double xPos, double yPos);
-	double * GetVelocity();
-	void SetVelocity(double xVel, double yVel);
+	Body(double xPos, double yPos, double bodyMass, double xVel, double yVel);
+
+	double GetMass() { return mass; };
+	double GetX() { return x; };
+	double GetY() { return y; };
+	void SetPosition(double xPos, double yPos) { x = xPos; y = yPos; };
+	double GetXVelocity() { return xVelocity; };
+	double GetYVelocity() { return yVelocity; };
+	void SetVelocity(double xVel, double yVel) { xVelocity = xVel; yVelocity = yVel; };
 };
 
-double Body::GetMass() {
-	// GetMass - returns mass of Body (double).
-	return mass;
-}
-
-double * Body::GetPosition() {
-	// GetPosition - returns array of xy position in the form [x,y].
-	static double position [2] = {x, y};
-	return position;
-}
-
-void Body::SetPosition(double xPos, double yPos) {
-	// SetPosition - Takes doubles xPos and yPos as parameters.
+Body::Body(double xPos, double yPos, double bodyMass, double xVel, double yVel) {
 	x = xPos;
 	y = yPos;
-}
-
-double * Body::GetVelocity() {
-	// GetVelocity - returns array of xVelocity and yVelocity.
-	static double velocity [2] = {xVelocity, yVelocity};
-	return velocity;
-}
-
-void Body::SetVelocity(double xVel, double yVel) {
-	// SetVelocity - Takes doubles xVel, and yVel.
+	mass = bodyMass;
 	xVelocity = xVel;
 	yVelocity = yVel;
+}
+
+int main() {
+	Body body = Body(1.0, 2.0, 30.0, 0.0, 0.0);
+
+	cout << "Mass: " << body.GetMass() << endl;
+	cout << "x: " << body.GetX() << ", y: " << body.GetY() << endl;
+	cout << "xVelocity: " << body.GetXVelocity() << ", yVelocity: " << body.GetYVelocity() << endl;
+
+	cout << "Setting Position to (10.0, -3.0)" << endl;
+
+	body.SetPosition(10.0, -3.0);
+
+	cout << "x: " << body.GetX() << ", y: " << body.GetY() << endl;
+
+	return 0;
 }
