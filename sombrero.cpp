5e76965e61120691f262dd293d14ca608c1bc163
Thomas Varnish <thomasvarnish@yahoo.co.uk>
2016-08-10 00:14:32 +0100

    Added unit definitions

--- sombrero.cpp
+++ sombrero.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+// Real World Units (https://www.iau.org/public/themes/measuring/)
+#define AU 149597870700 // Astronomical Unit - Average Earth-Sun Distance
+#define LYR 9.4607305e15 // Lightyear
+#define PC 3.0857e12 // Parsec
+#define MPC 1.0e6 * PC // Megaparsec
+// A Parsec is the distance to a star that subtends 1 arcsecond, at an arc
+// length of 1 AU.
+#define DAY 86400 // Day in seconds
+#define YR 365.25 * DAY // Year in Seconds
+#define M 1.9891e30 // Mass of the Sun
+
 using namespace std;
 
 // Body Class
@@ -34,7 +45,7 @@ int main() {
 
 	for (int i = 0; i < bodyCount; i++)
 	{
-		bodyArray[i] = new Body(0.0, i, 10.0, 0.0, 0.0);
+		bodyArray[i] = new Body(AU, i, 5.0e24, 1.0e3, 0.0);
 	}
 
 	cout << bodyArray[0]->GetX() << endl;
