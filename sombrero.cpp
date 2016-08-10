deb7d8465dc28811cd6de6033b27b81584280e13
Thomas Varnish <thomasvarnish@yahoo.co.uk>
2016-08-10 23:28:55 +0100

    Added LoadFromFile

--- sombrero.cpp
+++ sombrero.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 // Real World Units (https://www.iau.org/public/themes/measuring/)
 #define AU 149597870700 // Astronomical Unit - Average Earth-Sun Distance
@@ -81,19 +82,86 @@ void Output::Save() {
 	outputFile.close();
 }
 
+void LoadFromFile(string filename, int & bodyCount, int & width, int & height, string & positionUnits, double & scale, string & massUnits, Body * bodyArray []) {
+	ifstream inputFile(filename);
+	string fileLine;
+
+	// Read in setup parameters
+	getline(inputFile, fileLine);
+	stringstream setup(fileLine);
+	string parameter;
+	string parameterArray [6];
+	int i = 0;
+
+	while (getline(setup, parameter, ','))
+	{
+		parameterArray[i] = parameter;
+		i++;
+	}
+
+	bodyCount = stoi(parameterArray[0]);
+	width = stoi(parameterArray[1]);
+	height = stoi(parameterArray[2]);
+	positionUnits = parameterArray[3];
+	scale = stod(parameterArray[4]);
+	massUnits = parameterArray[5];
+
+	// Read in body details
+	int bodyIndex = 0;
+
+	while (getline(inputFile, fileLine))
+	{
+		stringstream bodyDetails(fileLine);
+		string detailArray [5];
+		i = 0;
+
+		while (getline(bodyDetails, parameter, ','))
+		{
+			detailArray[i] = parameter;
+			i++;
+		}
+
+		double x = stod(detailArray[0]);
+		double y = stod(detailArray[1]);
+		double mass = stod(detailArray[2]);
+		double xVelocity = stod(detailArray[3]);
+		double yVelocity = stod(detailArray[4]);
+
+		bodyArray[bodyIndex] = new Body(x, y, mass, xVelocity, yVelocity);
+		bodyIndex++;
+	}
+}
+
 int main() {
 	int bodyCount = 3;
 	int width = 100, height = 100;
-
-	Output dataOutput = Output("save.txt", bodyCount, width, height, "m", 1.0, "kg");
+	string positionUnits, massUnits;
+	double scale;
 
 	Body * bodyArray [bodyCount];
 
+	LoadFromFile("save.txt", bodyCount, width, height, positionUnits, scale, massUnits, bodyArray);
+	cout << bodyCount << endl;
+	cout << width << endl;
+	cout << height << endl;
+	cout << positionUnits << endl;
+	cout << scale << endl;
+	cout << massUnits << endl;
+
+	Output dataOutput = Output("save1.txt", bodyCount, width, height, "m", 1.0, "kg");
+
+	for (int i = 0; i < bodyCount; i++)
+	{
+		dataOutput.AddBody(bodyArray[i]);
+	}
+
+	/*
 	for (int i = 0; i < bodyCount; i++)
 	{
 		bodyArray[i] = new Body(AU * i, i, 5.0e24, 1.0e3, 0.0);
 		dataOutput.AddBody(bodyArray[i]);
 	}
+	*/
 
 	dataOutput.Save();
 
