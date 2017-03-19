2bac8b477fe0a102517c55d19c79eefe4401ad6d
Thomas Varnish <thomasvarnish@yahoo.co.uk>
2017-03-19 22:36:00 +0000

    Updated run length and timestep for test data

--- src/testing.cpp
+++ src/testing.cpp
@@ -4,15 +4,18 @@
 using namespace std;
 
 int main() {
+	// Create simulation object
 	Simulation sim = Simulation();
 
+	// Set simulation parameters
 	sim.SetSimulationName("HelloWorld");
 	sim.SetTimestep(YR / 365);
 	sim.SetFramerate(60);
 	sim.SetScale(AU, 100);
 
+	// Simulate setup
 	if (sim.LoadBodiesFromFile("init/realsolarsystem.csv")) {
-		sim.Run(0, 365);
+		sim.Run(0, 365, "Building Video...");
 	}
 	else {
 		cout << "Error in reading body file." << endl;
